419a85e390d91561aba9ec3cc4e9813a915e5f86
klauss1977 <klauss1977@yahoo.com>
2023-11-06 11:41:29 +0200

    final commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from ball import Ball
 from paddle import RightPaddle, LeftPaddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -13,16 +14,18 @@ screen.tracer(0)
 right_paddle = RightPaddle()
 left_paddle = LeftPaddle()
 ball = Ball()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkeypress(right_paddle.move_up, "Up")
 screen.onkeypress(right_paddle.move_down, "Down")
 screen.onkeypress(left_paddle.move_up, "w")
 screen.onkeypress(left_paddle.move_down, "s")
 screen.onkey(screen.bye, "q")
+game_is_on = True
 
-while True:
+while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     if ball.distance(right_paddle) < 40 and ball.xcor() > 320 or ball.distance(left_paddle) < 40 and ball.xcor() < -320:
         ball.bounce_x()
@@ -30,8 +33,10 @@ while True:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    if ball.xcor()>380:
+    if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
-    if ball.xcor()<-380:
+    if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
